03fd5a13031c28347d62dc95f9df1bfd6610e0c7
Romain Valtier <romain@valtier.fr>
2023-02-08 17:21:46 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget -c https://github.com/betaflight/betaflight-configurator/releases/download/10.9.0/betaflight-configurator_10.9.0_linux64-portable.zip -O betaflight-configurator_linux64-portable.zip
+
+unzip betaflight-configurator_linux64-portable.zip -d Betaflight-Configurator
