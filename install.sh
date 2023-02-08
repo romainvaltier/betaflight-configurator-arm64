c0c59cc1f8f98eb5fe79dbb10c3881567da29836
Romain Valtier <romain@valtier.fr>
2023-02-08 22:45:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
-wget -q -c https://github.com/betaflight/betaflight-configurator/releases/download/10.9.0/betaflight-configurator_10.9.0_linux64-portable.zip -O betaflight-configurator_linux64-portable.zip
-
-unzip -qq betaflight-configurator_linux64-portable.zip
-
-mv 'Betaflight Configurator' 'Betaflight-Configurator'
-
-nwArmVersion = nw60-arm64_2022-01-08
-wget -q https://github.com/LeonardLaszlo/nw.js-armv7-binaries/releases/download/$nwArmVersion/$nwArmVersion.tar.gz
-
-tar -xvf nw60-arm64_2022-01-08.tar.gz usr/docker/dist/nwjs-chromium-ffmpeg-branding/nwjs-v0.60.1-linux-arm64.tar.gz
-tar -xvf ./usr/docker/dist/nwjs-chromium-ffmpeg-branding/nwjs-v0.60.1-linux-arm64.tar.gz
-cd nwjs-v0.60.1-linux-arm64/
-cp -R * ../Betaflight-Configurator/
-
+bfVersion=10.9.0
+nwArmVersion=nw60-arm64_2022-01-08
 LINUX_INSTALL_DIR=/opt/betaflight
 metadata_name=betaflight-configurator
+
+wget -q -c https://github.com/betaflight/betaflight-configurator/releases/download/${bfVersion}/betaflight-configurator_${bfVersion}_linux64-portable.zip
+unzip -qq betaflight-configurator_${bfVersion}_linux64-portable.zip
+
+wget -q https://github.com/LeonardLaszlo/nw.js-armv7-binaries/releases/download/${nwArmVersion}/${nwArmVersion}.tar.gz
+tar -xvf ${nwArmVersion}.tar.gz usr/docker/dist/nwjs-chromium-ffmpeg-branding/nwjs-v0.60.1-linux-arm64.tar.gz
+tar -xvf ./usr/docker/dist/nwjs-chromium-ffmpeg-branding/nwjs-v0.60.1-linux-arm64.tar.gz
+cp -R ./nwjs-v0.60.1-linux-arm64/* ./'Betaflight Configurator'/
+
 rm -rf ${LINUX_INSTALL_DIR}/${metadata_name}
+mv ./'Betaflight Configurator'/ ${LINUX_INSTALL_DIR}/${metadata_name}
 chown root:root ${LINUX_INSTALL_DIR}
 chown -R root:root ${LINUX_INSTALL_DIR}/${metadata_name}
-# xdg-desktop-menu install ${LINUX_INSTALL_DIR}/${metadata_name}/${metadata_name}.desktop
+cp ${LINUX_INSTALL_DIR}/${metadata_name}/${metadata_name}.desktop /usr/share/applications/
 chmod +xr ${LINUX_INSTALL_DIR}/${metadata_name}/chrome_crashpad_handler
 chmod +xr ${LINUX_INSTALL_DIR}/${metadata_name}/${metadata_name}
 chmod -R +Xr ${LINUX_INSTALL_DIR}/${metadata_name}/
